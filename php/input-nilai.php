6ccc405ed01062467feec20482fb87fb5684e2f2
afz204 <afz60.30@gmail.com>
2017-05-28 14:17:28 +0700

    perbaikan input nilai

--- php/input-nilai.php
+++ php/input-nilai.php
@@ -71,6 +71,13 @@ $data = new Admin();
     $interview = $info['kd_interview'];
 ?>
 
+<div class="x_panel">
+    <div class="well">
+        <h5 class="page-header">KETENTUAN NILAI</h5>
+        <p>GRADE nilai A = 4, B = 3, C = 2, D = 1. Penilaian untuk masing-masing kategori yaitu "total kriteria / banyaknya kriteria".</p>
+    </div>
+</div>
+
 <div class="row">
     <div class="col-md-6">
         <div class="x_panel">
@@ -95,10 +102,10 @@ $data = new Admin();
                     <div class="form-group">
                     <select name="txt_nilai" class="form-control">
                         <option value="0" selected>GRADE</option>
-                        <option value="A">=> A</option>
-                        <option value="B">=> B</option>
-                        <option value="C">=> C</option>
-                        <option value="D">=> D</option>
+                        <option value="4">=> A</option>
+                        <option value="3">=> B</option>
+                        <option value="2">=> C</option>
+                        <option value="1">=> D</option>
                     </select>
             </div>
                     <button type="submit" name="addTest" class="btn btn-sm btn-danger"><span class="fa fa-fw fa-plus"></span></button>
@@ -112,23 +119,48 @@ $data = new Admin();
                     </thead>
                     <tbody>
                     <?php
-                        $qr = "SELECT * FROM tb_hasil_test WHERE kd_test = :kd";
+                        $qr = "SELECT id, nama_penilaian, nilai FROM tb_hasil_test WHERE kd_test = :kd";
                         $soq = $data->runQuery($qr);
                         $soq->execute(array(
                             ':kd'   => $test
                         ));
+                        $data = array();
+                       $sum = 0;
                         while ($row = $soq->fetch(PDO::FETCH_LAZY)){
-                    ?>
+                            $n = $row['nilai'];
+                            $sum +=$n;
+                            $data[] = $row;
+                         ?>
                     <tr>
-                        <td width="70%"><?php echo $row['nama_penilaian'];?></td>
+                        <td width="60%"><?php echo $row['nama_penilaian'];?></td>
                         <td width="20%"><?php echo $row['nilai'];?></td>
-                        <td width="10%">
+                        <td width="20%">
                             <a href="php/delete-nilai-test.php?kode=<?php echo $row['id'];?>&id=<?php echo $id;?>" onclick="return confirm('Are you sure you want to delete this item?');">
                                 <button class="btn btn-xs btn-danger"><span class="fa fa-fw fa-trash"></span></button>
                             </a>
                         </td>
                     </tr>
-                    <?php } ?>
+                        <?php
+                        }
+                        $total = count($data);
+                        $total = $sum/$total;
+                        if($total > 0 && $total < 2){
+                            $grade = "D";
+                        } elseif($total = 2 && $total < 3 ){
+                            $grade = "C";
+                        } elseif($total = 3 && $total < 4){
+                            $grade = "B";
+                        } elseif($total = 4){
+                            $grade = "A";
+                        }else{
+                            $grade = "null";
+                        }
+                        ?>
+                    <tr>
+                        <td>Total Nilai:</td>
+                        <td><?php echo $sum; ?></td>
+                        <td>GRADE: <?php echo $grade; ?></td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
@@ -157,10 +189,10 @@ $data = new Admin();
                     <div class="form-group">
                         <select name="txt_nilai" class="form-control">
                             <option value="0" selected>GRADE</option>
-                            <option value="A">=> A</option>
-                            <option value="B">=> B</option>
-                            <option value="C">=> C</option>
-                            <option value="D">=> D</option>
+                            <option value="4">=> A</option>
+                            <option value="3">=> B</option>
+                            <option value="2">=> C</option>
+                            <option value="1">=> D</option>
                         </select>
                     </div>
                     <button type="submit" name="addInterview" class="btn btn-sm btn-danger"><span class="fa fa-fw fa-plus"></span></button>
@@ -174,23 +206,48 @@ $data = new Admin();
                     </thead>
                     <tbody>
                     <?php
+                    $data = new Perusahaan;
                     $qr = "SELECT * FROM tb_hasil_interview WHERE kd_interview = :kd";
                     $soq = $data->runQuery($qr);
                     $soq->execute(array(
                         ':kd'   => $interview
                     ));
+                    $data = array();
+                    $sum = 0;
                     while ($row = $soq->fetch(PDO::FETCH_LAZY)){
+                        $n = $row['nilai'];
+                        $sum +=$n;
+                        $data[] = $row;
                         ?>
                         <tr>
-                            <td width="70%"><?php echo $row['nama_penilaian'];?></td>
+                            <td width="60%"><?php echo $row['nama_penilaian'];?></td>
                             <td width="20%"><?php echo $row['nilai'];?></td>
-                            <td width="10%">
+                            <td width="20%">
                                 <a href="php/delete-nilai-interview.php?kode=<?php echo $row['id'];?>&id=<?php echo $id;?>" onclick="return confirm('Are you sure you want to delete this item?');">
                                     <button class="btn btn-xs btn-danger"><span class="fa fa-fw fa-trash"></span></button>
                                 </a>
                             </td>
                         </tr>
-                    <?php } ?>
+                    <?php }
+                    $total = count($data);
+                    $total = $sum/$total;
+                    if($total > 0 && $total < 2){
+                        $grade = "D";
+                    } elseif($total = 2 && $total < 3 ){
+                        $grade = "C";
+                    } elseif($total = 3 && $total < 4){
+                        $grade = "B";
+                    } elseif($total = 4){
+                        $grade = "A";
+                    }else{
+                        $grade = "null";
+                    }
+                    ?>
+                    <tr>
+                        <td>Total Nilai:</td>
+                        <td><?php echo $sum; ?></td>
+                        <td>GRADE: <?php echo $grade; ?></td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
